f05a1bcfcbbcbbdb67f4437b227ff9608deb4512
Alexandra Adeeva <sashulka.a23@yandex.ru>
2023-09-14 01:50:03 +0500

    Files for a fact and request added

---
+++ NewTestTask/Models/Fact.swift
@@ -0,0 +1,8 @@
+//
+//  Fact.swift
+//  NewTestTask
+//
+//  Created by Александра Адеева on 14.09.2023.
+//
+
+import Foundation
---
+++ NewTestTask/Models/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  NewTestTask
+//
+//  Created by Александра Адеева on 14.09.2023.
+//
+
+import Foundation
