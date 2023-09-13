39f286d8901386ae6def9641673b818fd5a7f764
Alexandra Adeeva <sashulka.a23@yandex.ru>
2023-09-14 01:05:53 +0500

    Initial Commit

---
+++ NewTestTask/NewTestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTestTaskApp.swift
+//  NewTestTask
+//
+//  Created by Александра Адеева on 14.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewTestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
